67707a5b36f3ece306c948bd0383dfb9e02d90f2
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-14 23:35:17 -0500

    initial commit

---
+++ BootcampRepository/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BootcampRepository.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
