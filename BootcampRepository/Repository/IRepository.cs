e05a7f5037a1335d4741f51d4e7464ac4d1500e3
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-15 00:01:03 -0500

    ready to implement views

---
+++ BootcampRepository/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampRepository.Repository
+{
+    public interface IRepository<T> where T : class
+    {
+        Task CreateAsync(T entity);
+    }
+}
---
+++ BootcampRepository/Repository/Repository.cs
@@ -0,0 +1,26 @@
+﻿using BootcampRepository.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampRepository.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext context;
+
+        public Repository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task CreateAsync(T entity)
+        {
+            if (entity == null)
+                throw new ArgumentNullException(nameof(entity));
+
+            context.Add(entity);
+            await context.SaveChangesAsync();
+        }
+    }
+}
