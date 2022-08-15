59e0728dbd654f00bdca1a5889ca7c2da637efa7
Gerson Azabache <gerson_dunk@hotmail.com>
2022-08-14 23:47:16 -0500

    first migration

---
+++ BootcampRepository/Models/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BootcampRepository.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext([NotNullAttribute] DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Student> Students { get; set; }
+    }
+}
